5bf6b70a88e79aa6edcc4341e34615c159395e3d
Amit152116Kumar <amit170103004@iitg.ac.in>
2023-05-30 17:37:19 +0530

    Added logger & sio event listener

--- python/ks_api_client/ks_api.py
+++ python/ks_api_client/ks_api.py
@@ -6,7 +6,9 @@ import socketio
 from urllib3 import make_headers
 import urllib.parse
 import requests
-
+import logging
+import sys
+import datetime
 from ks_api_client.exceptions import ApiException, ApiValueError
 from ks_api_client.models import NewMTFOrder, NewNormalOrder, NewOrder, \
                 NewSMOrder, NewSOROrder, ExistingMTFOrder, ExistingNormalOrder, \
@@ -354,7 +356,7 @@ class KSTradeApi():
         return new_array
 
 
-    def subscribe(self, input_tokens, callback, broadcast_host=broadcast_host):
+    def subscribe(self, input_tokens, callback,connect_event, disconnect_event, broadcast_host=broadcast_host):
         try:
             if self.consumer_secret == None or not self.consumer_secret:
                 raise ApiValueError("Please provide the consumer_secret paramater while creating KSTradeApi object or supply in settings file.")
@@ -386,21 +388,43 @@ class KSTradeApi():
             if 'result' in jsonResponse and 'token' in jsonResponse['result'] and jsonResponse['result']['token']:
                 parsed_broadcast_host = urllib.parse.urlparse(broadcast_host)
                 socketio_path = parsed_broadcast_host.path
+                
+                date_now = datetime.date.today().strftime("%d%m%y")
+                # Set up a logging handler that writes logs to a file
+                logging_handler = logging.FileHandler(f'logs/socketio_{date_now}.log')
+                logging_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+                logging_handler.setLevel(logging.DEBUG)
+
+                # Set up the logging configuration for the socketio 
+
+                socketio_logger = logging.getLogger('socketio')     
+                socketio_logger.addHandler(logging_handler)
+                socketio_logger.setLevel(logging.DEBUG)
+
+                # Set up the logging configuration for the engineio 
+
+                engineio_logger = logging.getLogger('engineio')     
+                engineio_logger.addHandler(logging_handler)
+                engineio_logger.setLevel(logging.DEBUG)
+
+                # Redirect stderr to the logging handler too, so that all error messages are logged
+                sys.stderr = logging_handler.stream
                 self.sio = socketio.Client(
-                    reconnection=True, request_timeout=20, reconnection_attempts=5, engineio_logger=True,
+                    reconnection=True, request_timeout=30, reconnection_attempts=5, engineio_logger=True,
                             logger=True,http_session=session, ssl_verify=session.verify)
 
                 @self.sio.event
                 def connect():
                     self.sio.emit('pageload', {'inputtoken': input_tokens})
+                    connect_event()
 
                 @self.sio.event
                 def connect_error(data):
-                    print("Connection failed")
+                    disconnect_event()
 
                 @self.sio.event
                 def disconnect():
-                    print('Connection closed')
+                    disconnect_event()
 
                 @self.sio.on('getdata')
                 def on_getdata(data, callback=callback):
